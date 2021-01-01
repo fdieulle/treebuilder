7ba201493ef2734c788a9ac1a3b4f8da48ec6212
fdieulle <fabien.dieulle@gmail.com>
2020-12-31 19:38:04 -0500

    Prepare package structure and setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+   with open('README.md', 'r', encoding='utf-8') as f:
+       return f.read()
+
+
+setup(
+    name='treebuilder',
+    version='0.1',
+    author='Fabien Dieulle',
+    author_email='fabiendieulle@hotmail.fr',
+    description='Build tree data model.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/fdieulle/treebuilder',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
