c23878f180284dcaa59541f7d6681430910824d9
fdieulle <fabien.dieulle@gmail.com>
2021-01-03 11:52:34 -0500

    Add documentation for cross function

--- treebuilder/cross.py
+++ treebuilder/cross.py
@@ -3,7 +3,33 @@ from typing import Any, Dict, List
 
 # Todo: maybe it should be better to returns an iterable instead of a list
 def cross(source: List[Dict[str, Any]], entry: str, values: List[Any]) -> List[Dict[str, Any]]:
-    if len(source) == 0: # Todo: Maybe not because 0 x N should be = 0 as it does for N x 0 here
+    """Cross source with values
+
+    Cross generate all combination between source items and values as `S x V`
+    where `S` is the source length and `V` the values length.
+
+    Examples:
+        >>> import treebuilder as tb
+        >>> x = [{'Name': 'foo'}, {'Name': 'bar'}]
+
+        >>> y = tb.cross(x, 'Value', [1, 2])
+        >>> print(y)
+
+        >>> y = tb.cross(x, 'Value', [1, 2, 3])
+        >>> print(y)
+
+        >>> y = tb.cross(x, 'Value', [1])
+        >>> print(y)
+
+    Args:
+        source (List[Dict[str, Any]]): Source list to cross.
+        entry (str): Entry key under which values are stored.
+        values (List[Any]): List of values to expand.
+
+    Returns:
+        List[Dict[str, Any]]: The crossed list with `length = S x V`.
+    """
+    if len(source) == 0: # Todo: Maybe not because 0 x V should be = 0 as it does for S x 0 here
         return [{ entry: v } for v in values]
     
     result = []
