41b5716d8f1954d0db8fbcbf0688c55a1c8ce254
fdieulle <fabien.dieulle@gmail.com>
2021-01-03 12:30:41 -0500

    Add TreeBuilder documentation

--- treebuilder/TreeBuilder.py
+++ treebuilder/TreeBuilder.py
@@ -13,38 +13,136 @@ from treebuilder.json import to_json
 
 
 class TreeBuilder:
+    """Tree bulider main class.
+    """
+    @property
+    def root(self):
+        """[Dict[str, Any]]: Gets the tree root."""
+        return self.__root
 
     def __init__(self):
-        self.root = {}
+        self.__root = {}
         self.__lexer = FilterLexer()
         self.__parser = FilterParser()
 
-    def set(self, xpath: str, value) -> 'TreeBuilder':
+    def set(self, xpath: str, value: Any) -> 'TreeBuilder':
+        """Set value for a tree sub set
+
+        Args:
+            xpath: (str): The xpath to extract tree sub set
+            value: (Any): The value to apply for each leaf found.
+
+        Examples:
+            >>> import treebuilder as tb
+            >>> builder = TreeBuilder()
+            >>> builder.expand('bookstore/book/title', ['Sapiens', 'Harry Potter', 'A time of Mercy'])
+            >>> builder.set('bookstore/book/is_in_stock', True)
+            >>> print(builder.root)
+            >>> builder.set('bookstore/book[title='Harry Potter']/price', 9.99)
+
+        Returns:
+            TreeBuilder: Returns the builder itself.
+        """
         return self.expand(xpath, [value])
 
     def expand(self, xpath: str, values: List[Any]) -> 'TreeBuilder':
+        """Expand the sub set tree with values
+
+        This fuction use the `treebuilder.expand`. The source list is the tree sub 
+        set selected by the given xpath and the last xpath tag is the entry key.
+        For more details see the `treebuilder.expand` function documentation.
+
+        Args:
+            xpath: (str): The xpath to extract tree sub set
+            value: (List[Any]): Values to apply for each leaf found.
+
+        Examples:
+            >>> import treebuilder as tb
+            >>> builder = TreeBuilder()
+            >>> builder.expand('bookstore/book/title', ['Sapiens', 'Harry Potter', 'A time of Mercy'])
+            >>> builder.expand('bookstore/book/id', [1, 2, 3, 4, 5, 6])
+            >>> builder.expand('bookstore/book[title='Harry Potter']/price', [9.99])
+            >>> print(builder.root)
+
+        Returns:
+            TreeBuilder: Returns the builder itself.
+        """
         entry, items = self.__get_items(xpath)
         items = expand(items, entry, values)
         self.__attach_items_to_tree(items, entry)
         return self
 
     def nest(self, xpath: str, values: List[Any]) -> 'TreeBuilder':
+        """Nest the sub set tree with values.
+
+        This fuction use the `treebuilder.nest`. The source list is the tree sub 
+        set selected by the given xpath and the last xpath tag is the entry key.
+        For more details see the `treebuilder.nest` function documentation.
+
+        Args:
+            xpath: (str): The xpath to extract tree sub set
+            value: (List[Any]): Values to apply for each leaf found.
+
+        Examples:
+            >>> import treebuilder as tb
+            >>> builder = TreeBuilder()
+            >>> builder.expand('bookstore/book/title', ['Sapiens', 'Harry Potter', 'A time of Mercy'])
+            >>> builder.nest('bookstore/book/id', [1, 2, 3, 4, 5, 6])
+            >>> builder.nest('bookstore/book[title='Harry Potter']/price', [9.99])
+            >>> print(builder.root)
+
+        Returns:
+            TreeBuilder: Returns the builder itself.
+        """
         entry, items = self.__get_items(xpath)
         items = nest(items, entry, values)
         self.__attach_items_to_tree(items, entry)
         return self
 
     def cross(self, xpath: str, values: List[Any]) -> 'TreeBuilder':
+        """Cross the sub set tree with values.
+
+        This fuction use the `treebuilder.cross`. The source list is the tree sub 
+        set selected by the given xpath and the last xpath tag is the entry key.
+        For more details see the `treebuilder.cross` function documentation.
+
+        Args:
+            xpath: (str): The xpath to extract tree sub set
+            value: (List[Any]): Values to apply for each leaf found.
+
+        Examples:
+            >>> import treebuilder as tb
+            >>> builder = TreeBuilder()
+            >>> builder.expand('bookstore/book/title', ['Sapiens', 'Harry Potter', 'A time of Mercy'])
+            >>> builder.cross('bookstore/book/copy_number', [1, 2])
+            >>> print(builder.root)
+
+        Returns:
+            TreeBuilder: Returns the builder itself.
+        """
         entry, items = self.__get_items(xpath)
         items = cross(items, entry, values)
         self.__attach_items_to_tree(items, entry)
         return self
 
     def to_xml(self, file_path: str, root: str = None, pretty: bool = True):
-        to_xml(self.root, file_path, root=root, pretty=pretty)
+        """Serialize the built tree to a XML file.
 
-    def to_json(self, file_path: str, root: str = None, pretty: bool = True):
-        to_json(self.root, file_path, pretty=pretty)
+        Args:
+            file_path (str): Xml file path
+            root (str, optional): Additional xml root if needed. Defaults to None.
+            pretty (bool, optional): Define if you want a human reading output or not. Defaults to True.
+        """
+        to_xml(self.__root, file_path, root=root, pretty=pretty)
+
+    def to_json(self, file_path: str, pretty: bool = True):
+        """Serialize the built tree to a JSON file.
+
+        Args:
+            file_path (str): JSON file path
+            pretty (bool, optional): Define if you want a human reading output or not. Defaults to True.
+        """
+        to_json(self.__root, file_path, pretty=pretty)
     
     def __get_tag_and_filter(self, step: str) -> Tuple[str, str]:
         split = step.split('[')
@@ -66,7 +164,7 @@ class TreeBuilder:
 
         result = []
         queue = deque()
-        queue.appendleft((0, self.root, None))
+        queue.appendleft((0, self.__root, None))
         while len(queue) > 0:
             index, node, parent = queue.pop()
 
