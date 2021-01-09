420dc9365753660b3ec5b34f2cdfb1f4a7485433
fdieulle <fabien.dieulle@gmail.com>
2021-01-09 13:48:03 -0500

    Migrate from unittest to pytest

--- tests/test_cross.py
+++ tests/test_cross.py
@@ -1,57 +1,56 @@
-import unittest
+from typing import List
 from treebuilder.cross import cross
 from treebuilder.expand import expand
 
 
-class TestCross(unittest.TestCase):
-    def test_with_empty_source(self):
-        result = cross([], 'Name', ['foo', 'bar', 'other'])
-        
-        self.__check(result, 'Name', 'foo', 'bar', 'other')
+def __check(items: List, key: str, *args):
+    assert [x[key] for x in items] == [x for x in args]
 
-    def test_with_empty_values(self):
-        result = expand([], 'Name', ['foo', 'bar', 'other'])
-        result = cross(result, 'Value', [])
 
-        self.assertTrue(len(result) == 0) # Because N x 0 = 0
-
-    def test_cross(self):
-        result = expand([], 'Name', ['foo', 'bar', 'other'])
-        result = cross(result, 'Value', [1, 2, 3])
-
-        self.__check(result, 'Name', 'foo', 'bar', 'other', 'foo', 'bar', 'other', 'foo', 'bar', 'other')
-        self.__check(result, 'Value', 1, 1, 1, 2, 2, 2, 3, 3, 3)
-
-    def test_cross_with_smaller_values(self):
-        result = expand([], 'Name', ['foo', 'bar', 'other'])
-        result = cross(result, 'Value', [1, 2])
-
-        self.__check(result, 'Name', 'foo', 'bar', 'other', 'foo', 'bar', 'other')
-        self.__check(result, 'Value', 1, 1, 1, 2, 2, 2)
-
-        result = expand([], 'Name', ['foo', 'bar', 'other'])
-        result = cross(result, 'Value', [1])
-
-        self.__check(result, 'Name', 'foo', 'bar', 'other')
-        self.__check(result, 'Value', 1, 1, 1)
-
+def test_with_empty_source():
+    result = cross([], 'Name', ['foo', 'bar', 'other'])
     
-    def test_cross_with_longer_values(self):
-        result = expand([], 'Name', ['foo', 'bar'])
-        result = cross(result, 'Value', [1, 2, 3])
-
-        self.__check(result, 'Name', 'foo', 'bar', 'foo', 'bar', 'foo', 'bar')
-        self.__check(result, 'Value', 1, 1, 2, 2, 3, 3)
-
-        result = expand([], 'Name', ['foo'])
-        result = cross(result, 'Value', [1, 2, 3])
-
-        self.__check(result, 'Name', 'foo', 'foo', 'foo')
-        self.__check(result, 'Value', 1, 2, 3)
-
-    def __check(self, items, key, *args):
-        self.assertListEqual([x[key] for x in items], [x for x in args])
+    __check(result, 'Name', 'foo', 'bar', 'other')
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_with_empty_values():
+    result = expand([], 'Name', ['foo', 'bar', 'other'])
+    result = cross(result, 'Value', [])
+
+    assert len(result) == 0 # Because N x 0 = 0
+
+
+def test_cross():
+    result = expand([], 'Name', ['foo', 'bar', 'other'])
+    result = cross(result, 'Value', [1, 2, 3])
+
+    __check(result, 'Name', 'foo', 'bar', 'other', 'foo', 'bar', 'other', 'foo', 'bar', 'other')
+    __check(result, 'Value', 1, 1, 1, 2, 2, 2, 3, 3, 3)
+
+
+def test_cross_with_smaller_values():
+    result = expand([], 'Name', ['foo', 'bar', 'other'])
+    result = cross(result, 'Value', [1, 2])
+
+    __check(result, 'Name', 'foo', 'bar', 'other', 'foo', 'bar', 'other')
+    __check(result, 'Value', 1, 1, 1, 2, 2, 2)
+
+    result = expand([], 'Name', ['foo', 'bar', 'other'])
+    result = cross(result, 'Value', [1])
+
+    __check(result, 'Name', 'foo', 'bar', 'other')
+    __check(result, 'Value', 1, 1, 1)
+
+
+def test_cross_with_longer_values():
+    result = expand([], 'Name', ['foo', 'bar'])
+    result = cross(result, 'Value', [1, 2, 3])
+
+    __check(result, 'Name', 'foo', 'bar', 'foo', 'bar', 'foo', 'bar')
+    __check(result, 'Value', 1, 1, 2, 2, 3, 3)
+
+    result = expand([], 'Name', ['foo'])
+    result = cross(result, 'Value', [1, 2, 3])
+
+    __check(result, 'Name', 'foo', 'foo', 'foo')
+    __check(result, 'Value', 1, 2, 3)
